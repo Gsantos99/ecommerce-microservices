8d9716eb24aff0fdf0e53d87be79e270e7bc99a2
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-06-21 13:23:32 -0300

    chore: remove app-orders module and its related files
    - Deleted package.json, prisma schema, and migration files.
    - Removed broker and middleware implementations.
    - Cleared out schemas and TypeScript configuration.

--- app-orders/src/broker/broker.ts
+++
@@ -1,9 +0,0 @@
-import amqp from "amqplib";
-
-if (!process.env.BROKER_URL) {
-  throw new Error("BROKER_URL must be configured.");
-}
-
-export const broker = (async () => {
-  return await amqp.connect(process.env.BROKER_URL!);
-})();
